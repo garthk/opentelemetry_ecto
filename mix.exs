896e27d2830714621cb93df20f09bcc4d27e2519
Garth Kidd <garth@amplified.ai>
2021-03-10 13:31:09 +1100

    Upgrade dependencies.

--- mix.exs
+++ mix.exs
@@ -41,8 +41,8 @@ defmodule OpentelemetryEcto.MixProject do
   defp deps do
     [
       {:telemetry, "~> 0.4.2"},
-      {:opentelemetry_api, "~> 0.5.0"},
-      {:opentelemetry, "~> 0.5.0"},
+      {:opentelemetry, "~> 0.6.0"},
+      {:opentelemetry_api, "~> 0.6.0"},
       {:ex_doc, "~> 0.21.0", only: [:dev], runtime: false},
       {:ecto_sql, ">= 3.0.0", only: [:test]},
       {:postgrex, ">= 0.15.0", only: [:test]}
